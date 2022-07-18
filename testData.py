08b8877f60081e2ac637fe32610f153d56ec3617
joyivan <ivanbear@163.com>
2022-07-18 18:04:11 +0800

    finsh data process that read pickle file and write in the filesystem with DIR

--- testData.py
+++ testData.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot as plt
 import pickle
 import re
 import os
-filelist =os.listdir("/home/joyivan/PycharmProjects/pythonProject1/data/cifar-10-batches-py")
-os.chdir("/home/joyivan/PycharmProjects/pythonProject1/data/cifar-10-batches-py")
+import cv2
+workDir="/home/joyivan/PycharmProjects/pythonProject1/data/cifar-10-batches-py"
+dataDir=os.path.join(workDir)
+print(dataDir)
+filelist =os.listdir(workDir)
+os.chdir(workDir)
 pattern="data_"
+def creatDir(Dir):
+    if not os.path.isdir(Dir):
+        os.mkdir(Dir)
 def unpickle(file):
     with open(file,'rb') as fo:
         dict_=pickle.load(fo,encoding="bytes")
@@ -14,16 +21,20 @@ temp_file=[]
 for i in filelist:
     if re.match(pattern,i):
         print('ok')
-
         temp_file.append(unpickle(i))
     else:
         print('fail')
+metaDict=unpickle('batches.meta')
+print(metaDict.keys())
+metaLabel_names=metaDict[b'label_names']
+os.chdir(os.path.join(dataDir,"train"))
+for i in temp_file:
+    for j in range(len(i[b'filenames'])):
+       image=i[b'data'][j].reshape((3,32,32))
+       image=image.transpose((1,2,0))
+       imageLabel=i[b'labels'][j]
+       imageFileName=i[b'filenames'][j]
+       creatDir(os.path.join(dataDir,"train",str(imageLabel)))
+       cv2.imwrite(os.path.join(dataDir,"train",str(imageLabel),str(imageFileName,'utf-8')),image)
 
-print(filelist)
-imageData=temp_file[0][b'data']
-image=imageData[0].reshape((3,32,32))
-image=image.transpose((1,2,0))
-print(image.shape)
-plt.imshow(image)
-plt.show()
-print(imageData[0].shape)
+print("task finished!")
