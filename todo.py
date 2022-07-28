8ccae2f0da605ac9f28b38fc9f140f81215553cf
joyivan <ivanbear@163.com>
2022-07-28 21:46:49 +0800

    learn close bag and decor

--- todo.py
+++ todo.py
@@ -3,17 +3,54 @@
 #test git push cach user and pwd
 #test mbp from  20220728
 import numpy as np
-import Image
+from PIL import Image
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import torch
+import time
+def decor(func):
+    def wrapper(*arg):
+        t1=time.time()
+        result=func(*arg)
+        t2=time.time()
+        print('waste time is {}'.format(t2-t1))
+        return result
+    return wrapper
+#add=decor(add)
+@decor
+def add(x,y):
+    return x+y
+def panduan(sex):
+    def func1(func):
+         def func2():
+             if sex=='boy':
+                 print('buy car!')
+             elif sex=='girl':
+                 print('buy cloth!')
+             return func()
+         return func2
+    return func1
+@panduan(sex='boy')
+def man():
+    print("好好上班man")
+@panduan(sex='girl')
+def woman():
+    print("好好上班woman")
 
-
+if __name__=='__main__':
+    '''
+    for i in range(3):
+        print(i)
+    print(add(1,3))
+    print(torch.cuda.is_available())
+'''
+man()
 #def readData()
-
-  xs=[]
-
-  for year in np.arange(1999,2016,1):
+woman()
+xs=[]
+'''
+for year in np.arange(1999,2016,1):
 
       for month in np.arange(1,13,1):
            print year,month
@@ -23,7 +60,7 @@ import matplotlib.pyplot as plt
               xs.append(pd.read_csv('DATA'+str(year)+str(month)+'_NSW1.csv'))
 
 
-  for i in np.arange(0,len(xs),1):
+for i in np.arange(0,len(xs),1):
       yy=xs[i]
       yyy=np.array(yy['TOTALDEMAND'])
       if i==0:
@@ -31,4 +68,4 @@ import matplotlib.pyplot as plt
       else :
          yyyy=np.concatenate((yyyy,yyy))
 
-
+'''
