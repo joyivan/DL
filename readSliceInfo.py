91158e8282dc1a7b194edbe47d94924e3fc646dc
joyivan <ivanbear@163.com>
2022-08-25 11:28:53 +0800

    sort slice and rename

--- readSliceInfo.py
+++ readSliceInfo.py
@@ -1,8 +1,48 @@
 import pydicom
-workDIR='f:\COVID-19\CT_MD'
-orginDIR='COVID-19_Cases'
-data0=pydicom.read_file(workDIR+'\\'+orginDIR+'\\P001\\'+'IM0004.dcm')
-data1=pydicom.dcmread(workDIR+'\\'+orginDIR+'\\P001\\'+'IM0004.dcm')
-print(data0)
-print('-'*70)
-print(data1)
+import argparse
+import os
+import glob
+
+work_dir='/media/joyivan/OLD/data/CT_MD_JPG/'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-workDIR', type=str, default=work_dir, help='gl')
+    parser.add_argument('-orginDIR', type=str, default='COVID-19_Cases', help='gl')
+    opt = parser.parse_args()
+    print(opt)
+
+    workDIR = os.path.join(opt.workDIR, opt.orginDIR)
+    print(workDIR)
+    for root, dirs, files in os.walk(workDIR):
+        for dir in dirs:
+            dimList = glob.glob(os.path.join(root, dir, '*.jpg'))
+            print('dir is ', dir)
+            sliceLocationFileNameList=[]
+            slicelocationList=[]
+            for i in dimList:
+            # print(dimList)
+                fileInfo=pydicom.read_file(i)
+                sliceLocation=fileInfo[0x0020,0x1041].value
+                sliceLocation=float(sliceLocation)
+                sliceLocationFileNameList.append((sliceLocation,i))
+                slicelocationList.append(sliceLocation)
+
+            #print(sliceLocationFileNameList)
+            #get kv k:sliceLocation v:fileName
+            sliceLocationFileNameListDict=dict((sliceLocationFileNameList))
+            #print(sliceLocationFileNameListDict)
+            slicelocationList=sorted(slicelocationList,reverse = True )
+            #print(slicelocationList)
+            fileNo=1
+            for j in slicelocationList:
+                #print("sliceList:"+str(j))
+                #print(sliceLocationFileNameListDict)
+
+                fileName=sliceLocationFileNameListDict[j]
+                part1,file=os.path.split(str(fileName))
+                #print("result:"+file)
+                print(f'dir is {pat1},filename is {file}')
+                os.rename(fileName,part1+'/check'+"IM"+str(fileNo).zfill(4)+'.jpg')
+                fileNo+=1
+            print(dir+'rename files:'+str(fileNo))
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import torch
-import numpy as np
-a=np.random.randint(2,3)
-#test12121212
-
-print(a)
+import pydicom
+work_dir='/media/joyivan/OLD/data/CT_MD_JPG'
+orginDIR='COVID-19_Cases'
+data0=pydicom.read_file(work_dir+'/'+orginDIR+'/P073/'+'IM0144.dcm')
+data1=pydicom.dcmread(work_dir+'/'+orginDIR+'/P073/'+'IM0144.dcm')
+print(data0[0x0020,0x1041].value)
