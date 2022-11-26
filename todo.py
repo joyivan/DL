35f5420dbd45396c68378744d058d2729caaddcc
joyivan <ivanbear@163.com>
2022-11-26 23:00:19 +0800

    countinue to learn transforms

--- todo.py
+++ todo.py
@@ -1,86 +1,63 @@
-#feel sad 11082205:kidacat
-#today is good day which comes from mbp
-#e212121212   big machine from 
-#test git push cach user and pwd
-#test mbp from  20220728
-#test from 512
+# ----------------------------------------------------------------------------
+# Title:   Scientific Visualisation - Python & Matplotlib
+# Author:  Nicolas P. Rougier
+# License: BSD
+# ----------------------------------------------------------------------------
 import numpy as np
-from PIL import Image
-import pandas as pd
-import os
+from fluid import Fluid, inflow
+from scipy.special import erf
 import matplotlib.pyplot as plt
-import torch
-import time
-class mian:
-    attr1='面'
-    def __init__(self,manager_name):
-        self.manager_name=manager_name
+import matplotlib.animation as animation
 
-def decor(func):
-    def wrapper(*arg):
-        t1=time.time()
-        result=func(*arg)
-        t2=time.time()
-        print('waste time is {}'.format(t2-t1))
-        return result
-    return wrapper
-#add=decor(add)
-@decor
-def add(x,y):
-    return x+y
-def panduan(sex):
-    print('panduan function:{}'.format(sex))
-    def func1(func):
-         print('func1 function:{}'.format(func))
-         print('in func1 funcName is {}'.format(func.__name__))
+shape = 256, 256
+duration = 500
+fluid = Fluid(shape, "dye")
+inflows = [inflow(fluid, x) for x in np.linspace(-np.pi, np.pi, 8, endpoint=False)]
 
-         def func2():
-             print('func2 function')
-             if sex=='boy':
-                 print('buy car!')
-             elif sex=='girl':
-                 print('buy cloth!')
-             return func()
-         return func2
-    return func1
-@panduan(sex='boy')
-def man():
-    print("好好上班man")
-@panduan(sex='girl')
-def woman():
-    print("好好上班woman")
+# Animation setup
+fig = plt.figure(figsize=(5, 5), dpi=100)
+ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+ax.set_xlim(0, 1)
+ax.set_xticks([])
+ax.set_ylim(0, 1)
+ax.set_yticks([])
+im = ax.imshow(
+    np.zeros(shape),
+    extent=[0, 1, 0, 1],
+    vmin=0,
+    vmax=1,
+    origin="lower",
+    interpolation="bicubic",
+    cmap=plt.cm.RdYlBu,
+)
 
-if __name__=='__main__':
-    '''
-    for i in range(3):
-        print(i)
-    print(add(1,3))
-    print(torch.cuda.is_available())
-'''
-one_mian=mian('李子荣')
-print(one_mian.attr1)
-print(one_mian.manager_name)
-man()
-#def readData()
-#woman()
-xs=[]
-'''
-for year in np.arange(1999,2016,1):
+# Animation scenario
+scenario = []
+for i in range(8):
+    scenario.extend([[i]] * 20)
+scenario.extend([[0, 2, 4, 6]] * 30)
+scenario.extend([[1, 3, 5, 7]] * 30)
 
-      for month in np.arange(1,13,1):
-           print year,month
-           if month<10 :
-              xs.append(pd.read_csv('DATA'+str(year)+'0'+str(month)+'_NSW1.csv'))
-           else :
-              xs.append(pd.read_csv('DATA'+str(year)+str(month)+'_NSW1.csv'))
+# Animation update
+def update(frame):
+    for i in scenario[frame % len(scenario)]:
+        inflow_velocity, inflow_dye = inflows[i]
+        fluid.velocity += inflow_velocity
+        fluid.dye += inflow_dye
+    divergence, curl, pressure = fluid.step()
+    Z = curl
+    Z = (erf(Z * 2) + 1) / 4
+
+    im.set_data(Z)
+    im.set_clim(vmin=Z.min(), vmax=Z.max())
+
+    text.set_text("Frame %d" % frame)
+    if frame in [30, 60, 90, 120, 150, 180, 210, 240]:
+        plt.savefig("../../figures/animation/fluid-animation-%03d.png" % frame, dpi=300)
+
+    return im, text
 
 
-for i in np.arange(0,len(xs),1):
-      yy=xs[i]
-      yyy=np.array(yy['TOTALDEMAND'])
-      if i==0:
-         yyyy=yyy
-      else :
-         yyyy=np.concatenate((yyyy,yyy))
-
-'''
+text = ax.text(0.01, 0.99, "Test", ha="left", va="top", transform=ax.transAxes)
+anim = animation.FuncAnimation(fig, update, interval=10, frames=duration)
+plt.show()
